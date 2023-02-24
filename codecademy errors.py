b009f638cc2f4b49d0a0c525bc64b263f8d7297b
DeddasA <126174271+DeddasA@users.noreply.github.com>
2023-02-24 17:43:20 -0300

    Update codecademy errors.py

--- codecademy errors.py
+++ codecademy errors.py
@@ -46,7 +46,24 @@ box.weight = - 10
  
 print('ass')
 
+instrument_catalog = {
+  'Marimba': 1999,
+  'Kora': 499,
+  'Flute': 899
+}
 
+def print_instrument_price(instrument):
+
+  # Write your code below:
+ if instrument in instrument_catalog:
+      print('The price of a ' + instrument + ' is ' + str(instrument_catalog[instrument]))
+    # Checkpoint 3
+ else:
+      raise KeyError( 'The item' + instrument + ' is not found in instrument catalog!')
+
+print_instrument_price('Marimba')
+print_instrument_price('Flute')
+print_instrument_price('Piano')
 
 
 
