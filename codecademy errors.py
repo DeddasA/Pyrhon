60bf6f464d7ff5165758522aecf02e8bf5787bce
DeddasA <126174271+DeddasA@users.noreply.github.com>
2023-02-25 11:16:15 -0300

    Update codecademy errors.py

--- codecademy errors.py
+++ codecademy errors.py
@@ -134,5 +134,29 @@ except TypeError:
 except Exception:
   print('Hit an exception other than KeyError or TypeError!') 
 
+# semi- fix
+
+
+instrument_prices = {
+  'Banjo': 200,
+  'Cello': 1000,
+  'Flute': 100,
+}
+
+def display_discounted_price(instrument, discount):
+  full_price = instrument_prices[instrument]
+  discount_percentage = discount / 100
+  discounted_price = full_price - (full_price * discount_percentage)
+  print("The instrument's discounted price is: " + str(discounted_price))
+
+instrument = 'Clarinet'
+discount = 20
+
+# Write your code below:
+try:
+    display_discounted_price(instrument, discount)
+except:
+    for instrument, value in instrument_prices.items():
+        display_discounted_price(instrument, discount)
 
 
