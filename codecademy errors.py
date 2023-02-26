84612203b85d77f5cc336b4dfaab3bffab011d24
DeddasA <126174271+DeddasA@users.noreply.github.com>
2023-02-25 22:16:13 -0300

    Update codecademy errors.py

--- codecademy errors.py
+++ codecademy errors.py
@@ -158,5 +158,41 @@ try:
 except:
     for instrument, value in instrument_prices.items():
         display_discounted_price(instrument, discount)
+customer_rewards = {
+  'Zoltan': 82570,
+  'Guadalupe': 29850,
+  'Mario': 17849
+}
+
+def display_rewards_account(customer):
+  # Write your code below:
+  rewards_number = customer_rewards[customer]
+
+
+customer = 'Joshua'
+try:
+   rewards_number = customer_rewards[customer]
+except KeyError:
+  print('The customer: ' + customer.upper()  + ',' + ' was not found in rewards program!')
+
+else:
+ print('Rewards account number is: ' + str(rewards_number))
+
+
+import database
+
+instrument = 'Kora'
+database.connect_to_database()
+
+try:
+  database.display_instrument_info(instrument)
+except KeyError:
+  print('Oh no! This instrument does not exist.')
+else:
+  print(instrument)
+# Write your code below:
+finally:
+  database.disconnect_from_database()
+
 
 
