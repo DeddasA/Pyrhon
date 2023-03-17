9200b2019294d7e7ec420dd3242d6560192575d5
DeddasA <126174271+DeddasA@users.noreply.github.com>
2023-03-17 18:25:30 -0300

    Update sets.py

--- codecademy/sets.py
+++ codecademy/sets.py
@@ -23,3 +23,87 @@ for tag in tag_set:
 for tags in bad_tags:
     tag_set.remove(tags)
 print(tag_set)
+
+
+
+#Union set
+
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth']}
+
+user_tag_data = {'Lowkey Space': ['party', 'synth', 'fast', 'upbeat'],
+                 'Retro Words': ['happy', 'electronic', 'fun', 'exciting'],
+                 'Wait For Limit': ['romance', 'chill', 'rap', 'rhythmic'], 
+                 'Stomping Cue': ['country', 'swing', 'party', 'instrumental']}
+
+# Write your code below!
+new_song_data = {}
+
+for key,value in song_data.items():
+    song_data_set = set(value)
+    user_tag_data_set =set(user_tag_data[key])
+    intersections = song_data_set & user_tag_data_set
+    print(intersections)
+    new_song_data[key] = song_data_set | user_tag_data_set
+print(new_song_data)
+print('       ')
+#Intersection set 
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_recent_songs = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+                     'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat']}
+
+# Write your code below!
+song_data_set_1 = set(song_data['Retro Words'])
+user_recent_songs_set= set(user_recent_songs['Lowkey Space'])
+tags_int = song_data_set_1 & user_recent_songs_set
+print(tags_int)
+
+recommended_songs = {}
+nl = []
+for key, val in song_data.items():
+    for tag in val:
+        if tag in tags_int:
+            if key not in user_recent_songs:
+                recommended_songs[key] = tag
+            
+
+   
+#Different set
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance', 'relationship'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_liked_song = {'Back To Art': ['pop', 'sad', 'emotional', 'relationship']}
+user_disliked_song = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth']}
+
+song_data_set_03 = set(song_data)
+song_data_set_diff_disliked = set(song_data['Retro Words'])
+user_liked_song_set = set(user_liked_song['Back To Art'])
+user_disliked_song_set = set(user_disliked_song['Retro Words'])
+tag_diff =user_liked_song_set - user_disliked_song_set
+count = 0
+recommended_song_1 = {}
+nl = []
+for key,value in song_data.items():
+    for song in value:
+        for tag in value:
+            if tag in tag_diff:
+                if key not in user_disliked_song and key not in user_liked_song:
+                    recommended_song_1[key] = tag
+print(recommended_song_1)
+    
